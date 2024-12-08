fd12815456031404677948cb84f4466f2a39daf1
softtrack <nickfish2020@gmail.com>
2024-12-08 15:35:24 +0300

    add vuetify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,6 @@
-<template>
-  <HelloWorld />
-</template>
-
-<script>
-import { defineComponent } from 'vue';
-
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
-  },
-});
+<script setup>
+// TODO
 </script>
+<template>
+  <div />
+</template>
