7f293e5e2b56e56a7f7f2578da1704233a61a4bb
softtrack <nickfish2020@gmail.com>
2025-01-04 09:27:33 +0300

    FL_8: add table view

--- src/store/index.js
+++ src/store/index.js
@@ -7,11 +7,19 @@ export default createStore({
     fetched: false,
   },
   actions: {
-    async getObjs({ state }) {
+    async getObjs({ state }, payload = {}) {
       state.fetched = false
+      state.objs = null
       try {
-        const response = await axios.get('https://fl.drros.ru/api/assets/')
+        const response = await axios.get('https://fl.drros.ru/api/assets/', {
+          params: {
+            ...payload
+          }
+        })
         state.objs = response.data
+      } catch (e) {
+        state.objs = null
+        state.fetched = true
       } finally {
         state.fetched = true
       }
